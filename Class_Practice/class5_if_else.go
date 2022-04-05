d11d40704e3143a9a265dda91ab3fd9548f748a4
Pritom Paul <pritompaul1920@gmail.com>
2022-04-05 12:07:17 +0600

    New class codes

--- Class_Practice/class5_if_else.go
+++ Class_Practice/class5_if_else.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	x := 20
 
+	fmt.Println("If-Else:-------------")
 	if x%2 == 0 {
 		if x%5 == 0 {
 			fmt.Println("Divisible by 2 and 5.")
@@ -13,5 +14,15 @@ func main() {
 		}
 	} else {
 		fmt.Println("Not divisible by 2")
+	}
+
+	println("Ladder:--------------")
+
+	if x%5 == 0 && x%2 == 0 {
+		fmt.Println("Divisible by 2 and 5!")
+	} else if x%2 == 0 && x%5 != 0 {
+		fmt.Println("Divisible by 2, but not  5!")
+	} else {
+		fmt.Println("Not divisible by 2!")
 	}
 }
